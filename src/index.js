3cd2485f5a5384a01afdf2ade654f2a6473a9a2e
saijayanth59 <sithlord5995@gmail.com>
2025-05-14 23:20:41 +0530

    fix cors

--- src/index.js
+++ src/index.js
@@ -7,34 +7,26 @@ const initDatabase = require("./utils/dbInit");
 const productRoutes = require("./routes/productRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
-
 const app = express();
-
 
 app.use(
   cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-
+app.use(express.json());
 app.use(morgan("dev"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 initDatabase();
-
 
 app.use("/api/products", productRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
